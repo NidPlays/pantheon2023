a7a89bd82e3cef12a508a5b43b92eb0a1422f196
R Tushar <dmrtushar@gmail.com>
2023-06-10 20:36:41 +0530

    build fcked up-2

--- pantheon2023/src/App.jsx
+++ pantheon2023/src/App.jsx
@@ -1,10 +1,8 @@
 import Navbar from "./components/Navbar"
-import HeroSection from "./components/HeroSection"
 export default function App() {
   return (
     <>
     <Navbar />
-    <HeroSection />
     </>
   )
 }
