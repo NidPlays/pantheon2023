6268585a77c082fa743b8a0d79879bd0ecce0aae
R Tushar <dmrtushar@gmail.com>
2023-06-11 00:42:40 +0530

    ~updated footer

--- pantheon2023/src/App.jsx
+++ pantheon2023/src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar"
 import HeroSection from "./components/HeroSection"
 import About from "./components/About"
 import Contact from "./components/Contact"
+import Footer from "./components/Footer"
 export default function App() {
   return (
     <>
@@ -10,6 +11,7 @@ export default function App() {
     <HeroSection />
     <About />
     <Contact />
+    <Footer />
     </div>
     </>
   )
--- pantheon2023/src/components/Footer.jsx
+++ pantheon2023/src/components/Footer.jsx
@@ -1,37 +1,36 @@
-import React from 'react'
+
 import { FaFacebook, FaTwitter, FaLinkedin, FaInstagram } from 'react-icons/fa'
 
 function Footer() {
     return(
         <div>
-  <footer class="footer">
-    <div class="waves">
-      <div class="wave" id="wave1"></div>
-      <div class="wave" id="wave2"></div>
-      <div class="wave" id="wave3"></div>
-      <div class="wave" id="wave4"></div>
+  <footer className="footer pb-5">
+    <div className="waves">
+      <div className="wave" id="wave1"></div>
+      <div className="wave" id="wave2"></div>
+      <div className="wave" id="wave3"></div>
+      <div className="wave" id="wave4"></div>
     </div>
-    <ul class="social-icon">
-      <li class="social-icon__item"><a class="social-icon__link" href="#">
+    <ul className="social-icon">
+      <li className="social-icon__item"><a className="social-icon__link" href="#">
         <FaFacebook />
         </a></li>
-      <li class="social-icon__item"><a class="social-icon__link" href="#">
+      <li className="social-icon__item"><a className="social-icon__link" href="#">
         <FaTwitter />
         </a></li>
-      <li class="social-icon__item"><a class="social-icon__link" href="#">
+      <li className="social-icon__item"><a className="social-icon__link" href="#">
         <FaLinkedin />
         </a></li>
-      <li class="social-icon__item"><a class="social-icon__link" href="#">
+      <li className="social-icon__item"><a className="social-icon__link" href="#">
         <FaInstagram />
         </a></li>
     </ul>
-    <ul class="menu">
-      <li class="menu__item"><a class="menu__link" href="#">Home</a></li>
-      <li class="menu__item"><a class="menu__link" href="#">About</a></li>
-      <li class="menu__item"><a class="menu__link" href="#">Services</a></li>
-      <li class="menu__item"><a class="menu__link" href="#">Team</a></li>
-      <li class="menu__item"><a class="menu__link" href="#">Contact</a></li>
-
+    <ul className="menu">
+      <li className="menu__item"><a className="menu__link" href="#">Home</a></li>
+      <li className="menu__item"><a className="menu__link" href="#">About</a></li>
+      <li className="menu__item"><a className="menu__link" href="#">Services</a></li>
+      <li className="menu__item"><a className="menu__link" href="#">Team</a></li>
+      <li className="menu__item"><a className="menu__link" href="#">Contact</a></li>
     </ul>
     <p>&copy;2021 Nadine Coelho | All Rights Reserved</p>
   </footer>
