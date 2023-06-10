82158e3cba70945ed60ebd8499947cf427f16b0e
Shodhan Shetty <Shodhanshetty144@gmail.com>
2023-06-10 17:28:03 +0530

    Navbar Component

--- pantheon2023/src/components/Navbar.jsx
+++ pantheon2023/src/components/Navbar.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>@Shodhan Navbar maduu!!</div>
+    <div>
+    <ul class="nav-links">
+    <li><a href="#">Dashboard</a></li>
+    <li><a href="#">Portfolio</a></li>
+    <li><a href="#">Services</a></li>
+    <li ><a href="#">Feedback</a></li>
+  </ul>
+    </div>
   )
 }
 
