e8d39af1b2d55bdf301928ba2751c736d11d130b
R Tushar <dmrtushar@gmail.com>
2023-06-10 19:29:41 +0530

    router setup done

--- pantheon2023/src/components/Navbar.jsx
+++ pantheon2023/src/components/Navbar.jsx
@@ -1,4 +1,9 @@
 
+import {MdOutlineHouse, MdEmojiEvents} from 'react-icons/md'
+import { GrGallery } from 'react-icons/gr'
+import { Link as a } from 'react-router-dom'
+import { Link as LinkS } from 'react-scroll'
+
 function Navbar() {
   return (
     <div className="navbar-container">
@@ -7,13 +12,13 @@ function Navbar() {
       </div>
       <ul className="nav-links ml-5">
         <li>
-          <a href="#">Home</a>
+          <a className='inline-flex'><MdOutlineHouse size={22} className='icon-margin' />&nbsp;Home</a>
         </li>
         <li>
-          <a href="#">Events</a>
+        <a className='inline-flex'><MdEmojiEvents size={22} className='icon-margin' />&nbsp;Events</a>
         </li>
         <li>
-          <a href="#">Gallery</a>
+        <a  className='inline-flex'><GrGallery size={22} className='icon-margin' />&nbsp;Gallery</a>
         </li>
       </ul>
     </div>
