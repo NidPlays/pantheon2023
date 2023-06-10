9fd65520e47317e3f4cc6aee2173e95fa71ee4e6
R Tushar <dmrtushar@gmail.com>
2023-06-10 17:58:18 +0530

    updated navbar

--- pantheon2023/src/components/Navbar.jsx
+++ pantheon2023/src/components/Navbar.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
-
+import Pantheon from "../assets/Pantheon.png"
 function Navbar() {
   return (
-    <div>
-    <ul class="nav-links">
-    <li><a href="#">Dashboard</a></li>
-    <li><a href="#">Portfolio</a></li>
-    <li><a href="#">Services</a></li>
-    <li ><a href="#">Feedback</a></li>
-  </ul>
+    <div className="navbar-container">
+      <div className="nav-image">
+      <img loading="lazy" src={Pantheon} className="ml-6 relative z-10 h-24 w-24" />
+      </div>
+      <ul className="nav-links">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Events</a>
+        </li>
+        <li>
+          <a href="#">Gallery</a>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
