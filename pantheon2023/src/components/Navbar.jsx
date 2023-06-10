9c4b9c47224a7475a63d2aa7837aa11b3f1b3de0
R Tushar <dmrtushar@gmail.com>
2023-06-10 19:08:30 +0530

    chaned the icon from img to text

--- pantheon2023/src/components/Navbar.jsx
+++ pantheon2023/src/components/Navbar.jsx
@@ -1,9 +1,9 @@
-import Pantheon from "../assets/Pantheon.png"
+
 function Navbar() {
   return (
     <div className="navbar-container">
-      <div className="nav-image">
-      <img loading="lazy" src={Pantheon} className="ml-6 relative z-10 h-28 w-28 mr-2" />
+      <div className="logo-text">
+        Pantheon
       </div>
       <ul className="nav-links ml-5">
         <li>
