a0390ae3e40d25fa7ac4723bc1c15f73df5cdac6
R Tushar <dmrtushar@gmail.com>
2023-06-10 21:09:52 +0530

    updated herosection with dummy vedio

---
+++ pantheon2023/src/components/HeroSection.jsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import Video from "../assets/backround_video.mp4";
+import Image from "../assets/Pantheon.png"
+
+const HeroSection = () => {
+  useEffect(() => {
+    Aos.init({ duration: 1500 });
+  });
+
+  return (
+    <div
+      className=" flex justify-center items-center h-screen absolute z-10 overflow-hidden top-0"
+      data-aos="fade-in"
+      id="home"
+    >
+      <video
+        className="custom-css-video"
+        autoPlay
+        loop
+        muted
+        src={Video}
+        type="video/
+          mp4"
+      />
+      <div className="bg-transparent z-10 max-w-7xl absolute py-2 px-6 flex flex-col items-center">
+        <div
+          className="bg-transparent text-white text-5xl font-semibold text-center font-Montserrat custom-css-hero-h1"
+          data-aos="fade-in"
+        >
+          <img src={Image} alt="" className="h-72 w-72 relative z-50 inline-flex mr-6" />
+        </div>
+        <div className="mt-8 flex flex-col items-center">
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
--- pantheon2023/src/components/Navbar.jsx
+++ pantheon2023/src/components/Navbar.jsx
@@ -2,16 +2,16 @@
 
 function Navbar() {
   return (
-    <nav className="bg-white">
-        <div className="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
+    <nav className="relative bg-transparent backdrop-blur-sm top-0 z-50">
+        <div className="container flex items-center justify-center p-3 mx-auto text-gray-600 capitalize dark:text-gray-300">
         <div className="logo-text md:text-2xl sm:text-base inline-flex justify-start">
         Pantheon
       </div>
-            <a href="#" className="border-b-2 border-transparent dark:hover:text-gray-200 hover:border-blue-500 transition-all mx-1.5 sm:mx-6">home</a>
+            <a href="#" className="text-white border-b-2 border-transparent hover:border-blue-500 transition-all mx-1.5 sm:mx-6">home</a>
     
-            <a href="#" className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 transition-all mx-1.5 sm:mx-6">Events</a>
+            <a href="#" className="text-white border-b-2 border-transparent hover:border-blue-500 transition-all mx-1.5 sm:mx-6">Events</a>
     
-            <a href="#" className="custom-line border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 transition-all mx-1.5 sm:mx-6">Gallery</a>
+            <a href="#" className="text-white custom-line border-b-2 border-transparent hover:border-blue-500 transition-all mx-1.5 sm:mx-6">Gallery</a>
         </div>
     </nav>
   );
