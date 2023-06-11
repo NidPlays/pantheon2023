417ee1fec33b5f4106e10afd7ce3d0d4b9facec3
Subramanyarao11 <subramanya11rao@gmail.com>
2023-06-11 20:57:06 +0530

    added another button

--- pantheon2023/src/components/About.jsx
+++ pantheon2023/src/components/About.jsx
@@ -33,7 +33,33 @@ function About() {
 
                 <div className="flex flex-col items-center gap-2 w-full lg:flex-row mt-8 mb-36 md:mb-2 lg:mb-2">
                     {/* 1st section */}
-                    <div className="w-1/2 flex-col">
+                    <div className="w-1/2 flex-col md:w-1/3 lg:w-1/3">
+                    <h1 className="mx-auto  max-w-4xl font-display text-3xl text-center font-medium tracking-tight text-slate-100 sm:text-4xl">
+                        Event{' '}
+                        <span className="relative whitespace-nowrap text-red-600">
+                            <svg
+                                aria-hidden="true"
+                                viewBox="0 0 418 42"
+                                className="absolute top-2/3 left-0 h-[0.58em] w-full fill-red-300/70"
+                                preserveAspectRatio="none"
+                            >
+                                <path d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z" />
+                            </svg>
+                            <span className="relative">Brochure</span>
+                        </span>{' '}
+                    </h1>
+                    <div className="text-center mt-4">
+                    <button className='inline-flex h-12 animate-background-shine items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50'>
+                        Click Me
+                    </button>
+                    </div>
+
+                    </div>
+
+                    {/* 1st section ends*/}
+
+                    {/* 2nd section */}
+                    <div className="w-1/2 flex-col md:w-1/3 lg:w-1/3">
                     <h1 className="mx-auto  max-w-4xl font-display text-3xl text-center font-medium tracking-tight text-slate-100 sm:text-4xl">
                         Buy{' '}
                         <span className="relative whitespace-nowrap text-red-600">
@@ -56,10 +82,13 @@ function About() {
 
                     </div>
 
-                    {/* 1st section ends*/}
-                    <div className="w-1/2 flex-col">
+
+                    {/* second section ends */}
+
+                    {/* Last section */}
+                    <div className="w-1/2 flex-col md:w-1/3 lg:w-1/3">
                     <h1 className="mx-auto  max-w-4xl font-display text-3xl text-center font-medium tracking-tight text-slate-100 sm:text-4xl">
-                        Event{' '}
+                        Register{' '}
                         <span className="relative whitespace-nowrap text-red-600">
                             <svg
                                 aria-hidden="true"
@@ -69,7 +98,7 @@ function About() {
                             >
                                 <path d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z" />
                             </svg>
-                            <span className="relative">Brochure!</span>
+                            <span className="relative">Events</span>
                         </span>{' '}
                     </h1>
                     <div className="text-center mt-4">
@@ -80,8 +109,8 @@ function About() {
 
                     </div>
 
-                    {/* 2nd section */}
-                    {/* second section ends */}
+
+                    {/* Last Section ends */}
                 </div>
             </div>
         </div>
