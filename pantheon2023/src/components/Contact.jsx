50c9fa2d469a3f521ec199e0bd587e695528ed5f
Nid <22031046+NidPlays@users.noreply.github.com>
2023-06-11 17:36:45 +0530

    Change color to red

--- pantheon2023/src/components/Contact.jsx
+++ pantheon2023/src/components/Contact.jsx
@@ -303,7 +303,7 @@ function Contact() {
                                     <div className="sm:col-span-2 sm:flex sm:justify-end">
                                         <button
                                             type="submit"
-                                            className="inline-flex items-center justify-center w-full px-6 py-3 mt-2 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:w-auto"
+                                            className="inline-flex items-center justify-center w-full px-6 py-3 mt-2 text-base font-medium text-white bg-red-500 border border-transparent rounded-md shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:w-auto"
                                         >
                                             Submit
                                         </button>
