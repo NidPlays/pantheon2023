079459c42ff749f60430dff030d5e2e1a99dd054
R Tushar <dmrtushar@gmail.com>
2023-06-11 12:48:06 +0530

    added new navbar

--- pantheon2023/src/components/Navbar.jsx
+++ pantheon2023/src/components/Navbar.jsx
@@ -1,20 +1,55 @@
 
+import { useState } from "react";
+import { ImCross } from "react-icons/im"
+import { FaBars } from "react-icons/fa"
+export default function NavBar() {
+    const [navbar, setNavbar] = useState(false);
 
-function Navbar() {
-  return (
-    <nav className="relative bg-transparent backdrop-blur-sm top-0 z-50">
-        <div className="container flex items-center justify-center p-3 mx-auto text-gray-600 capitalize dark:text-gray-300">
-        <div className="logo-text inline-flex justify-start">
-        Pantheon
-      </div>
-            <a href="#" className="custom-css-nav-text text-white border-b-2 border-transparent hover:border-blue-500 transition-all mx-1.5 sm:mx-6">home</a>
-    
-            <a href="#" className="custom-css-nav-text text-white border-b-2 border-transparent hover:border-blue-500 transition-all mx-1.5 sm:mx-6">Events</a>
-    
-            <a href="#" className="custom-css-nav-text text-white custom-line border-b-2 border-transparent hover:border-blue-500 transition-all mx-1.5 sm:mx-6">Gallery</a>
-        </div>
-    </nav>
-  );
+    return (
+        <nav className="w-full shadow relative bg-transparent backdrop-blur-sm top-0 z-50">
+            <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+                <div>
+                    <div className="flex items-center justify-between py-3 md:py-5 md:block">
+                        <a href="javascript:void(0)">
+                            <h2 className="text-2xl logo-text font-bold">Pantheon</h2>
+                        </a>
+                        <div className="md:hidden">
+                            <button
+                                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                                onClick={() => setNavbar(!navbar)}
+                            >
+                                {!navbar ? (
+                                  <FaBars size={20} className="text-white" />
+                                ) : (
+                                  <ImCross size={20} className="text-white" />
+                                )}
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div
+                        className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+                            navbar ? "block" : "hidden"
+                        }`}
+                    >
+                        <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+                            <li className="text-white ">
+                                <a href="javascript:void(0)">Home</a>
+                            </li>
+                            <li className="text-white ">
+                                <a href="javascript:void(0)">Events</a>
+                            </li>
+                            <li className="text-white ">
+                                <a href="javascript:void(0)">About us</a>
+                            </li>
+                            <li className="text-white ">
+                                <a href="javascript:void(0)">Gallery</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
 }
-
-export default Navbar;
