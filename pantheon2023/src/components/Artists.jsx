8401b9e60052c6b0a4068ac623143f677a1555e3
Subramanyarao11 <subramanya11rao@gmail.com>
2023-06-11 23:33:07 +0530

    added dj prithvi

--- pantheon2023/src/components/Artists.jsx
+++ pantheon2023/src/components/Artists.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import shivu from "../assets/shivu.jpg";
 import brodhav from "../assets/brodhav.jpeg";
+import djprithvi from "../assets/djprithvi.jpg";
 
 function Artists() {
     return (
@@ -117,11 +118,65 @@ function Artists() {
                         <div className="z-10 hidden mt-4 ml-3 lg:ml-4 lg:mt-4 lg:block">
                             <img className="w-full" src={shivu} alt />
                         </div>
+
+
                         {/* <div className="block w-full lg:hidden">
                         <img className="w-full" src="https://i.ibb.co/nBpjXrT/1.png" alt />
                     </div> */}
                     </div>
+
                 </div>
+                                        {/* Testing */}
+                                        <div className="container px-6 py-12 mx-auto">
+                            <div className="flex flex-col items-center lg:flex-row lg:justify-between pb-14">
+                                <div className="relative flex w-1/2 mb-5 lg:mb-0">
+                                    <div className>
+                                        <div className="absolute top-0 z-0 hidden lg:block">
+                                            <svg className xmlns width={403} height={527} viewBox="0 0 403 527" fill="none">
+                                                <rect
+                                                    x="-1.61167"
+                                                    y="1.37931"
+                                                    width={362}
+                                                    height={497}
+                                                    transform="matrix(-0.996996 -0.077453 -0.077453 0.996996 399.607 28.1496)"
+                                                    stroke="#E97451"
+                                                    strokeWidth={3}
+                                                />
+                                            </svg>
+                                        </div>
+                                    </div>
+                                    <div className="z-10 hidden mt-4 ml-3 lg:ml-4 lg:mt-4 lg:block">
+                                        <img className="w-full object-cover" src={djprithvi} alt />
+                                    </div>
+                                    <div className="block w-full lg:hidden">
+                                        <img className="w-full object-cover" src={djprithvi} alt />
+                                    </div>
+                                </div>
+                                <div className="flex items-center lg:w-1/2">
+                                    <div className="hidden lg:block lg:pr-6">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width={151} height={4} viewBox="0 0 151 4" fill="none">
+                                            <line x1="0.75" y1="1.63696" x2="150.75" y2="1.63696" stroke="#E97451" strokeWidth={3} />
+                                        </svg>
+                                    </div>
+                                    <div className="lg:pt-0 lg:pt-5">
+                                        <div className="py-4 text-2xl font-normal text-center text-gray-200 lg:text-left f-f-d-s lg:text-3xl xl:text-5xl">
+                                            DJ Prithvi
+                                        </div>
+                                        <div className="py-3 text-base font-normal text-center text-gray-100 xl:text-2xl lg:text-left xl:leading-9 f-f-l">
+                                        DJ Prithvi is an up-and-coming DJ known for his versatility and the ability to blend various genres seamlessly, creating a unique and energetic atmosphere in his sets. Whether it's EDM, house, hip-hop, or Bollywood remixes, he brings a fresh and dynamic approach to his performances.
+                                        </div>
+                                        <div className="flex justify-center w-full lg:block">
+                                            <a target="_blank" href="https://www.instagram.com/djprithvi_/" rel="noreferrer">
+                                                <button className="pt-8 text-base font-bold text-white border-b-2 border-red-500 lg:text-2xl focus:outline-none f-f-l">
+                                                    View Artist
+                                                </button>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        {/* Testing */}
             </div>
         </div>
     );
