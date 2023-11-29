83004f5423650d638e7ff1dc617d856c78280870
Subramanyarao11 <subramanya11rao@gmail.com>
2023-11-29 13:18:25 +0530

    added OG Image

--- pantheon2023/src/components/SEO.jsx
+++ pantheon2023/src/components/SEO.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Helmet } from 'react-helmet-async';
+import OGImage from "../assets/OG.png";
 
 export const SEO = ({ title, description, keywords, meta = [], canonicalUrl }) => {
   const staticMeta = [
@@ -13,7 +14,15 @@ export const SEO = ({ title, description, keywords, meta = [], canonicalUrl }) =
     ...meta,
   ];
 
-  const allMeta = [...staticMeta, ...dynamicMeta];
+  const ogMeta = [
+    { property: "og:type", content: "website" },
+    { property: "og:url", content: "https://pantheon2023.nidish.xyz/" }, //change after configuring domain
+    { property: "og:title", content: title },
+    { property: "og:description", content: description },
+    { property: "og:image", content: OGImage },
+  ];
+
+  const allMeta = [...staticMeta, ...dynamicMeta , ...ogMeta];
 
   return (
     <Helmet
