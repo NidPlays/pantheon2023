011bbe97a638803ca4a4f1b35915b4974b8d0ef5
AUB <adit.ignite@gmail.com>
2023-12-15 13:51:42 +0530

    schedule done

--- pantheon2023/src/main.jsx
+++ pantheon2023/src/main.jsx
@@ -1,30 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HelmetProvider } from 'react-helmet-async';
-import EventsPage from '../src/pages/EventsPage.jsx'
-import AboutUsPage from '../src/pages/AboutUsPage.jsx'
-import GalleryPage from '../src/pages/GalleryPage.jsx'
-import Error from '../src/components/Error.jsx'
-import EventDetailsPage from './pages/EventDetailsPage.jsx';
-import ThankYou from './components/ThankYou.jsx';
-import './index.css'
+import { HelmetProvider } from "react-helmet-async";
+import EventsPage from "../src/pages/EventsPage.jsx";
+import AboutUsPage from "../src/pages/AboutUsPage.jsx";
+import GalleryPage from "../src/pages/GalleryPage.jsx";
+import Error from "../src/components/Error.jsx";
+import EventDetailsPage from "./pages/EventDetailsPage.jsx";
+import ThankYou from "./components/ThankYou.jsx";
+import "./index.css";
+import EventSchedulePage from "./pages/EventSchedulePage.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <BrowserRouter>
-        <HelmetProvider>
-            <Routes>
-                <Route exact path='/' element={<App />} />
-                <Route path="/events" element={<EventsPage />} />
-                <Route path="/gallery" element={<GalleryPage />} />
-                <Route path="/aboutus" element={<AboutUsPage />} />
-                <Route path="events/:id" element={<EventDetailsPage />} />
-                <Route path="/thankyou" element={<ThankYou />} />
-                <Route path="*" element={<Error />} />
-            </Routes>
-        </HelmetProvider>
-        </BrowserRouter>
-    </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <HelmetProvider>
+        <Routes>
+          <Route exact path="/" element={<App />} />
+          <Route path="/events" element={<EventsPage />} />
+          <Route path="/gallery" element={<GalleryPage />} />
+          <Route path="/aboutus" element={<AboutUsPage />} />
+          <Route path="/schedule" element={<EventSchedulePage />} />
+
+          <Route path="events/:id" element={<EventDetailsPage />} />
+          <Route path="/thankyou" element={<ThankYou />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </HelmetProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
---
+++ pantheon2023/src/pages/EventSchedulePage.jsx
@@ -0,0 +1,271 @@
+import Navbar from "../components/Navbar";
+import React from "react";
+import Footer2 from "../components/Footer2";
+import Logos from "../components/Logos";
+
+const EventSchedulePage = () => {
+  return (
+    <div>
+      <main className=" bg-[#151515] text-white p-8 max-w-7xl mx-auto">
+        <Navbar />
+        <style>{"body { background-color: #151515; }"}</style>
+        <h1 className="font-poppins text-4xl mb-8">Schedule</h1>
+
+        {/* First Section */}
+        <section className="h-auto mb-8">
+          <h2 className="text-red-500 font-poppins font-semibold text-2xl">
+            15 December
+          </h2>
+          <br />
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">Saraswathi</h3>
+              <p className="text-gray-300">11:00 AM - 12:30 PM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">
+                Mythological Reincarnation
+              </h3>
+              <p className="text-gray-300">12:00 PM - 2:00 PM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">
+                Kronos’ Time Trials
+              </h3>
+              <p className="text-gray-300">2:00 PM - 5:00 PM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">
+                Celestial Contrast
+              </h3>
+              <p className="text-gray-300">12:00 PM - 1:30 PM</p>
+              {/*<img src="https://images.fineartamerica.com/images-medium-large-5/maroon-bells-aspen-colorado-black-and-white-photography-by-sai.jpg" alt="Black and white photo of a lake" className="w-full rounded-md" />*/}
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">
+                Hayagreeva’s Verdict
+              </h3>
+              <p className="text-gray-300">3:00 PM - 5:00 PM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">
+                Epic Conquest
+              </h3>
+              <p className="text-gray-300">12:00 PM - 4:30 PM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">
+                Suicide Chess Saga
+              </h3>
+              <p className="text-gray-300">3:00 PM - 5:00 PM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">
+                Battle of Titans
+              </h3>
+              <p className="text-gray-300">12:00 PM - 2:00 PM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">Mono Act</h3>
+              <p className="text-gray-300">1:00 PM - 2:30 PM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">
+                Classical Solo Music
+              </h3>
+              <p className="text-gray-300">12:00 PM - 2:00 PM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">
+                Newspaper Vogue
+              </h3>
+              <p className="text-gray-300">2:00 PM - 4:00 PM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">
+                Fitness Witness
+              </h3>
+              <p className="text-gray-300">2:00 PM - 4:30 PM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">
+                Fashion Show
+              </h3>
+              <p className="text-gray-300">4:00 PM - 6:00 PM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">
+                Reels Featuring Pantheon
+              </h3>
+              <p className="text-gray-300">11 AM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">
+                Anime Quiz{" "}
+              </h3>
+              <p className="text-gray-300">2:00 PM - 4:30 PM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">
+                Photography
+              </h3>
+              <p className="text-gray-300">10:00 AM - 11:00 AM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">
+                Group Dance
+              </h3>
+              <p className="text-gray-300">2:00 PM - 4:00 PM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">
+                Western Dance Solo
+              </h3>
+              <p className="text-gray-300">12:30 PM - 2:30 PM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">
+                Where is Waldo{" "}
+              </h3>
+              <p className="text-gray-300">12:30 PM - 2:00 PM</p>
+            </article>
+          </div>
+        </section>
+
+        <section className="h-auto mb-8">
+          <h2 className="text-red-500 font-poppins font-semibold text-2xl">
+            16 December
+          </h2>
+          <br />
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">
+                Western Vocal Solo music
+              </h3>
+              <p className="text-gray-300">1:00 PM - 3:00 PM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">
+                Dumb charades
+              </h3>
+              <p className="text-gray-300">10:00 AM - 12:00 PM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">
+                Fandom Quiz
+              </h3>
+              <p className="text-gray-300">1:00 PM - 3:00 PM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">
+                Face painting
+              </h3>
+              <p className="text-gray-300">11:00 AM - 1:00 PM</p>
+              {/*<img src="https://images.fineartamerica.com/images-medium-large-5/maroon-bells-aspen-colorado-black-and-white-photography-by-sai.jpg" alt="Black and white photo of a lake" className="w-full rounded-md" />*/}
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">Shark tank</h3>
+              <p className="text-gray-300">10:00 AM - 1:00 PM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">
+                Semi Classical Solo
+              </h3>
+              <p className="text-gray-300">12:00 PM - 2:00 PM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">
+                Intrumental music
+              </h3>
+              <p className="text-gray-300">10:00 AM - 1:00 PM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">
+                Street play
+              </h3>
+              <p className="text-gray-300">11:00 AM - 2:00 PM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">
+                Beg Borrow Steal{" "}
+              </h3>
+              <p className="text-gray-300">12:00 PM - 2:00 PM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">
+                Battle of Bands
+              </h3>
+              <p className="text-gray-300">1:00 PM - 3:00 PM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">Stand Up</h3>
+              <p className="text-gray-300">1:00 PM - 3:00 PM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">
+                Beat Boxing{" "}
+              </h3>
+              <p className="text-gray-300">10:00 AM - 12:00 PM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">
+                Indian Cinema Music
+              </h3>
+              <p className="text-gray-300">10:00 AM - 12:00 PM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">Valorant </h3>
+              <p className="text-gray-300">12:00 PM - 2:00 PM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">BGMI </h3>
+              <p className="text-gray-300">10:00 AM - 2:30 PM</p>
+            </article>
+
+            <article className="pb-4 border-b border-gray-700">
+              <h3 className="font-poppins font-semibold text-lg">Smoked 23</h3>
+              <p className="text-gray-300">11:00 PM - 3:30 PM</p>
+            </article>
+          </div>
+        </section>
+      </main>
+      <Logos />
+      {/* New footer removed old one */}
+      <Footer2 />
+    </div>
+  );
+};
+
+export default EventSchedulePage;
